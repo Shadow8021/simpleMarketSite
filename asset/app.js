f6e58034466660d6bed49adccb96adf41736e00d
shadow <gregoyaga@gmail.com>
2025-09-19 00:25:46 +0200

    mon exercice 3

--- asset/app.js
+++ asset/app.js
@@ -1,10 +1,19 @@
 //CREE UNE VARIABLE GLOBALE ET AFFICHER DANS LA FONCTION
-let nom = "OYAGA"
-var prenom = "martial"
-function afficherVariable(){
-    console.log(nom);
-    
+// let nom = "OYAGA"
+// var prenom = "martial"
+// function afficherVariable(){
+//     console.log(nom);    
+// }
+// afficherVariable()
+// console.log(prenom);
+
+
+
+
+//utilisation de let et var dans une boucle
+
+for(var i=0;i<10;i++){
+    console.log("bonjour",i);
     
 }
-afficherVariable()
-console.log(prenom);
+console.log("notre variable est",i);
