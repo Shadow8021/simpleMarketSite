8ea953453802a06f75828263ccfcaa9100af451d
shadow <gregoyaga@gmail.com>
2025-09-19 01:07:00 +0200

    modified:   asset/app.css 	modified:   asset/app.js 	modified:   index.html

--- asset/app.js
+++ asset/app.js
@@ -1,1 +1,10 @@
- 
+//CREE UNE VARIABLE GLOBALE ET AFFICHER DANS LA FONCTION
+let nom = "OYAGA"
+var prenom = "martial"
+function afficherVariable(){
+    console.log(nom);
+    
+    
+}
+afficherVariable()
+console.log(prenom);
