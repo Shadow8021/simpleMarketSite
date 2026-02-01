52791a5c433c1054529d92e10c9a04d8fccaf505
shadow <gregoyaga@gmail.com>
2026-02-01 15:40:54 +0100

    mise a jour des donnees des produits

--- src/components/Produits/data.js
+++ src/components/Produits/data.js
@@ -5,43 +5,50 @@ import Image5 from "../../assets/img/5.jpg"
 import Image6 from "../../assets/img/6.jpg"
 import Image7 from "../../assets/img/7.jpg"
 import Image8 from "../../assets/img/8.jpg"
+import Image9 from "../../assets/img/9.jpg"
 
 export let donnees = [
     {
         nom:"PC Gamer",
         desc:"Rizen 7x",
-        prix:659,
+        prix:899,
         image:Image2
+        
+    },{
+        nom:"PC Gamer",
+        desc:"AMD Core 7",
+        prix:749,
+        image:Image3
         
     },{
         nom:"PC Gamer",
         desc:"Rizen 7x",
         prix:659,
-        image:Image2
+        image:Image4
         
     },{
         nom:"PC Gamer",
-        desc:"Rizen 7x",
-        prix:659,
-        image:Image2
+        desc:"Rizen 5x",
+        prix:859,
+        image:Image5
         
     },{
-        nom:"PC Gamer",
-        desc:"Rizen 7x",
-        prix:659,
-        image:Image2
+        nom:"Clavier",
+        desc:"Touche mécanique",
+        prix:89,
+        image:Image6
         
     },{
-        nom:"PC Gamer",
-        desc:"Rizen 7x",
-        prix:659,
-        image:Image2
+        nom:"Ecran PC",
+        desc:`24"Full HD`,
+        prix:239,
+        image:Image7
         
     },{
-        nom:"PC Gamer",
-        desc:"Rizen 7x",
+        nom:"Carte Graphique",
+        desc:"Rizen 5x",
         prix:659,
-        image:Image2
+        image:Image9
         
     }
 ]
