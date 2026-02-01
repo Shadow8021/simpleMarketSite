a245f4d17dc2d635f511d7d94817e9009a461365
shadow <gregoyaga@gmail.com>
2026-02-01 15:49:03 +0100

    ajustement des données

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Contenaire from '../Contenaire'
 export default function Hero() {
   return (
-        <section className="containte">
+
+          <section className="containte">
             <h1>Nouveautés</h1>
             <p>Notre nouveau catalogue avec les technologies les plus avancées</p>
             <a href="#produits" className="btn0">DECOUVRIR</a>
         </section>
+     
     
   )
 }
